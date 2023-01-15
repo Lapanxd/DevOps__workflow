36a1fb8d0164ac1f32fd48bc5caf011bb37af9c0
Lapanxd <morgan.dumas@outlook.fr>
2023-01-15 17:17:31 +0100

    q5

--- SimpleMath.js
+++ SimpleMath.js
@@ -2,6 +2,10 @@ class SimpleMath {
     static addition(a, b) {
         return a + b;
     }
+
+    static soustraction(a, b) {
+        return a - b;
+    }
 }
 
 module.exports = SimpleMath;
--- SimpleMath.spec.js
+++ SimpleMath.spec.js
@@ -4,3 +4,6 @@ test('adds 1 + 2 to equal 3', () => {
   expect(SimpleMath.addition(1, 2)).toBe(3);
 });
   
+test('soustrait 5 - 3 to equals 2', () => {
+  expect(SimpleMath.soustraction(5, 3)).toBe(2);
+});
